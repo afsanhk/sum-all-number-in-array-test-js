7cb61ffdc219185d566a62952bd6dcb24dc1faa3
Afsanul Haque Khan <afsanhk@gmail.com>
2021-06-25 20:32:54 +0000

    Updated index.js to add all elements in any number of nested arrays

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+  let sum = 0
+  array.forEach(element => {
+    if (Array.isArray(element)) {
+      sum += sumItems(element);
+    } else {
+      sum += element; 
+    }
+  });
+  return sum;
 }
+
+
+
+let data1 = [1, 2, 3, 4, 5];
+let data2 = [[1, 2, [[3], 4]], 5, []];
+let data3 = [[[[[[[[[[[[[1]]]]]]]]]]]]];
+
+let result1 = sumItems(data1);
+let result2 = sumItems(data2);
+let result3 = sumItems(data3);
+
+
+console.log(`Result for ${data1} is ${result1}`);
+console.log(`Result for ${data2} is ${result2}`);
+console.log(`Result for ${data3} is ${result3}`);
 
 module.exports = sumItems;
